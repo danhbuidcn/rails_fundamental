64c5141982fe2a9aa4a07019cad94c62c7f5df4d
danhbuidcn <danhbuidcn@gmail.com>
2024-03-07 10:24:33 +0700

    issue-9: adding a secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-#   has_secure_password
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i\
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  has_secure_password
 
   before_save { self.email = email.downcase }
 
   validates :name, presence: true, length: { maximum: 255 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'validations' do
-    subject(:user) { build(:user) }
+    subject(:user) { build(:user, password: "123456") }
 
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_length_of(:name).is_at_most(255) }
@@ -14,5 +14,7 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_uniqueness_of(:email) }
     it { is_expected.to allow_value('example@example.com').for(:email) }
     it { is_expected.not_to allow_value('invalid_email').for(:email) }
+
+    it { is_expected.to validate_presence_of(:password) }
   end
 end
