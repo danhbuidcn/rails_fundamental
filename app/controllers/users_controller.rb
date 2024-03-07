a73651724e954c0dfb10b3089f0494fb78fb1289
danhbuidcn <danhbuidcn@gmail.com>
2024-03-07 14:14:13 +0700

    issue-9: create user sign ip form, docker compose - enables debugging within containers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,29 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  def new; end
+  skip_before_action :verify_authenticity_token
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(create_params)
+    if @user.save
+      flash[:success] = 'Welcome to the Sample App!'
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def create_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'validations' do
-    subject(:user) { build(:user, password: "123456") }
+    subject(:user) { build(:user, password: '123456') }
 
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_length_of(:name).is_at_most(255) }
