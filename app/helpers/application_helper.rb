67f57b8da447543e71947acd913e57c9b24b2915
Bùi Thượng <60874663+danhbuidcn@users.noreply.github.com>
2024-08-14 01:52:00 +0000

    add flash message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  # Returns the appropriate Bootstrap alert class based on the flash type
+  def flash_class(type)
+    Constants::FLASH_CLASSES[type] || 'alert-primary' # Fallback class
+  end
 end
