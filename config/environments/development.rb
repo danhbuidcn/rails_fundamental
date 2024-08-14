eafd874c7850ab0a944172dba15724f10b83129e
Bùi Thượng <60874663+danhbuidcn@users.noreply.github.com>
2024-08-14 08:32:30 +0000

    custom css for page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,6 +61,11 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Don't cache assets
+  config.assets.debug = true
+  config.assets.compile = true
+  config.assets.digest = false
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
